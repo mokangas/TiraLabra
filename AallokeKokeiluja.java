c1eee3b7c1c90e953d126069150718c59d897d66
mokangas <mikko.kangasmaki@helsinki.fi>
2013-05-30 23:18:52 +0300

    Viikkoraportti 3 ja aallokekokeiluja

--- AallokeKokeiluja.java
+++ AallokeKokeiluja.java
@@ -8,140 +8,157 @@ import javax.imageio.ImageIO;
 
 public class AallokeKokeiluja {
 
+    private static final int BLUE = 0;
+    private static final int GREEN = 1;
+    private static final int RED = 2;
+    private static final int NO_OF_COLORS = 3;
+
     public static void main(String[] args) throws IOException {
-
-        short[][] data = ekstraktoiData("c.bmp");
-        int[][] aallokkeet = summadata(data);
-        // kirjoitaTiedostoon(aallokkeet);
-        //System.out.println(tarkistaSummat(aallokkeet));;
-        
+        File f = new File("c.bmp");
+        byte[][][] data = readFileIntoByteData(f);
+        int[] a = muunnaSarakeAallokkeiksi(data[0][0]);
+        tulostaBytetJaSummaTaulukko(data[0][0], a);
     }
 
-    public static boolean tarkistaSummat(int[][] luvut) {
+    public static int[] muunnaSarakeAallokkeiksi(byte[] data) {
 
-        for (int i = 0; i < luvut.length; i++) {
-            if (!tarkistaRivinSumma(luvut[i])) {
-                return false;
+        int koko = data.length;
+        int[] summat = summaaSarake(data);
+        int[] aallokkeet = new int[koko];
+
+        koko = koko / 2;
+
+        for (int i = 0; i < koko; i++) {
+            aallokkeet[i] = data[2 * i] - data[2 * i + 1];
+        }
+
+        int osoitin = koko;
+        koko = koko / 2;
+        while (koko >= 1) {
+            for (int i = 0; i < koko / 2; i++) {
+                aallokkeet[osoitin + i] = summat[osoitin - koko + 2 * i] + summat[osoitin - koko + 2 * i + 1];
+            }
+            koko = koko / 2;
+            osoitin = osoitin + koko;
+        }
+
+
+        return aallokkeet;
+    }
+
+    public static byte[][][] readFileIntoByteData(File file) throws IOException {
+
+        BufferedImage image = ImageIO.read(file);
+        byte[][][] bgrData = new byte[NO_OF_COLORS][image.getWidth()][image.getHeight()];
+
+        for (int y = 0; y < image.getHeight(); y++) {
+            for (int x = 0; x < image.getWidth(); x++) {
+                for (int color = 0; color < NO_OF_COLORS; color++) {
+                    bgrData[color][x][y] = bgrIntegerToColor(image.getRGB(x, y), color);
+                }
             }
         }
 
-        return true;
+        return bgrData;
     }
 
-    private static boolean tarkistaRivinSumma(int[] rivi) {
-        int summa = 0;
-
-        for (int j = 0; j < rivi.length / 2; j++) {
-            summa += rivi[j];
-        }
-
-        int kohta = rivi.length / 2;
-        int pituus = kohta / 2;
-
-        while (pituus >= 1) {
-            int apusumma = 0;
-            for (int i = 0; i < pituus; i++) {
-                apusumma += rivi[kohta];
-                kohta++;
-            }
-            if (apusumma != summa) {
-                System.out.println(apusumma+"\t"+ summa);
-                return false;
-            }
-            pituus = pituus / 2;
-        }
-        
-        return true;
+    public static byte bgrIntegerToColor(int rgbInt, int color) {
+        rgbInt = rgbInt >> (8 * color); // shift the relevant bits to the end
+        rgbInt = rgbInt & 0XFF;          // ignore all the other bits
+        rgbInt = rgbInt - 128;           // so that casting into bytes doesn't break distances
+        return (byte) rgbInt;
     }
 
-    public static void kirjoitaTiedostoon(int[][] data) throws IOException {
-
-        FileWriter fw = new FileWriter(new File("summat.txt"));
-
+    public static void printByteData(byte[][][] data) {
         for (int i = 0; i < data.length; i++) {
             for (int j = 0; j < data[0].length; j++) {
-                fw.write(" " + data[i][j]);
+                for (int k = 0; k < data[0][0].length; k++) {
+                    System.out.print(" " + data[i][j][k]);
+                }
+                System.out.println("");
             }
-            fw.write("\n");
         }
-
-        fw.close();
     }
 
-    private static short[][] ekstraktoiData(String filename) throws IOException {
-        File f = new File(filename);
-        BufferedImage image = ImageIO.read(f);
-        short[][] data = new short[image.getWidth()][image.getHeight()];
+    /**
+     * Laskee summia byte-sarakkeesta: ensiksi kaksi byteä kerrallaan palautus
+     * taulukkoon, sitten edellisten perään neljä byteä kerrallaan, niiden
+     * perään kahdeksan byteä kerrallaan jne,
+     *
+     * @param data
+     * @return
+     */
+    public static int[] summaaSarake(byte[] data) {
 
-        for (int x = 0; x < image.getWidth(); x++) {
-            for (int y = 0; y < image.getHeight(); y++) {
-                data[x][y] = (short) (image.getRGB(x, y) & 0xFF);
-            }
+        int koko = data.length;
+        int[] summat = new int[koko];
+        int osoitin = 0;
+
+        // ensin kahden summat:
+        for (int i = 0; i < koko; i++) {
+            summat[i / 2] += data[i];
         }
 
+        // Sitten loput:
+        osoitin = koko / 2;
+        koko = koko / 2;
+
+        while (koko >= 1) {
+            for (int i = 0; i < koko / 2; i++) {
+                //System.out.println(""+koko+" "+ (osoitin+i)+"\t");
+                summat[osoitin + i] = summat[osoitin - koko + 2 * i] + summat[osoitin - koko + 2 * i + 1];
+            }
+            koko = koko / 2;
+            osoitin += koko;
+        }
+
+        return summat;
+    }
+
+    public static byte[] palautaAlkuperainen(int[] aallokkeet, int pituus){
+        
+        byte[] data = new byte[pituus];
+        int bitteja = 1;
+        int osoitin = 0;
+        
+        
         return data;
     }
-
-    private static int[][] summadata(short[][] data) {
-
-        int[][] aallokkeet = new int[data.length][data[0].length];
-        for (int i = 0; i < aallokkeet.length; i++) {
-            summaaRivi(aallokkeet[i], data[i]);
-        }
-
-        return aallokkeet;
-    }
-
-    private static void summaaRivi(int[] summa, short[] data) {
-
-        // Summataan ensin datat inttiin:
-        for (int i = 0; i < data.length / 2; i++) {
-            summa[i] = data[2 * i] + data[2 * i + 1];
-        }
-
-        // Sitten jatketaan:
-        int pituus = data.length / 4;
-        int kohta = data.length / 2;
-
-        while (pituus >= 1) {
-            for (int i = 0; i < pituus; i++) {
-                summa[kohta] = summa[kohta - 2 * pituus] + summa[kohta - 2 * pituus + 1];
-                kohta++;
-            }
-            pituus = pituus / 2;
-
-        }
-    }
     
-    public static int[][] aallokkeetTheHardWay(int[] data){
-        
-        int koko = data.length;
-        
-        int[][] aallokkeet = new int[koko/2][koko/2];
-        
-        for (int i = 0; i < koko/2; i++) {
-            for (int j = 0; j < i; j++) {
-                
-            }
-        }
-        
-        return aallokkeet;
-    }
     
     /**
-     * Palauttaa luvun k, jolle
-     * k = max{ m : 2^m <= n}
-     * @param n
-     * @return 
+     * suurin k s.e. 2^k <= n.
      */
-    public static int log2(int n){
-        
-        int i = 0;
-        while ( n >= 1){
-            i++;
-            n = n/2;
+    public static int log2(int n) {
+        int log = -1;
+        while (n >= 1) {
+            log++;
+            n = n / 2;
         }
-        
-        return i-1;
+        return log;
+    }
+
+    public static void tulostaBytetJaSummaTaulukko(byte[] bytet, int[] summat) throws IOException {
+
+        FileWriter bw = new FileWriter(new File("bytet.txt"));
+        FileWriter sw = new FileWriter(new File("summat.txt"));
+        for (int i = 0; i < summat.length; i++) {
+            bw.write(" " + bytet[i]);
+        }
+
+        int koko = summat.length / 2;
+        int osoitin = 0;
+        while (koko > 1) {
+            for (int i = 0; i < koko; i++) {
+                // sw.write((osoitin + i) + ":" + summat[osoitin + i] + " "); // numero ja arvo.
+                sw.write(" "+ summat[osoitin+i]); // Pelkät arvot
+            }
+            sw.write("\n======================\n");
+            osoitin = osoitin + koko;
+            koko = koko / 2;
+        }
+
+        bw.close();
+        sw.close();
     }
 }
