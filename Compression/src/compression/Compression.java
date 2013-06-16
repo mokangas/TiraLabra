0e003bf51db34b202ef86943efd3fd79e6ec536c
mokangas <mikko.kangasmaki@helsinki.fi>
2013-06-16 17:57:51 +0300

    palautus

--- Compression/src/compression/Compression.java
+++ Compression/src/compression/Compression.java
@@ -3,15 +3,22 @@ package compression;
 import GUI.GraphicalUI;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Compression {
 
-    
+    private static String STATS_TEMP_WTF_FILE_NAME = "statTempWtfFile.wtf";
+    private static String STATS_TEMP_BMP_FILE_NAME = "statTempBmpFile.wtf";
+    private static int STATS_MIN_LOL = 0;
+    private static int STATS_MAX_LOL = 20;
+
     /**
-     * Launches the gui or chooses the action according to the command line arguments.
+     * Launches the gui or chooses the action according to the command line
+     * arguments.
+     *
      * @param args Check the manual.
-     * @throws IOException 
+     * @throws IOException
      */
     public static void main(String[] args) throws IOException {
 
@@ -27,7 +34,11 @@ public class Compression {
                 }
                 break;
             case 2:
-                commandLineFromWtfToBmp(args);
+                if (args[0].trim().equals("writeStats")) {
+                    writeStatistics(args[1]);
+                } else {
+                    commandLineFromWtfToBmp(args);
+                }
                 break;
             case 3:
                 commandLineFromBmpToWtf(args);
@@ -221,5 +232,91 @@ public class Compression {
      */
     private static void launchGui() {
         GraphicalUI gui = new GraphicalUI();
+    }
+
+    /**
+     * Does a series of transforms and analyzses them.
+     *
+     * @param imgName
+     * @throws IOException
+     */
+    private static void writeStatistics(String imgName) throws IOException {
+
+        FileWriter writer = new FileWriter(new File("Stats-" + imgName.substring(0, imgName.indexOf(".")) + ".txt"));
+        writer.write("Transform with lol = " + STATS_MIN_LOL + " - " + STATS_MAX_LOL + " performed on the image " + imgName);
+        writer.write("\n=====================================================================================================");
+
+        long time = System.currentTimeMillis();
+        File img = new File(imgName);
+        byte[][][] data = BitmapIO.readFileIntoByteData(img);
+        time = System.currentTimeMillis() - time;
+
+        writer.write("\nReading data took " + time + " milliseconds. ");
+        writer.write("Original file size "+(img.length()/1000)+" kb. ");
+        writer.write("Dimensions "+data[0].length + " x " +data[0][0].length+" (width x height)");
+        writer.write("\n=====================================================================================================");
+        writer.write("\nlol \twtf size\tTransform \tWriting wtf\treading wtf \tinverse transform \twriting bmp");
+
+        for (int lol = STATS_MIN_LOL; lol < STATS_MAX_LOL; lol++) {
+            long[] stats = convertAndAnalyze(data, lol);
+            writer.write("\n" + lol + "\t" + (stats[5]/1000) + " kb.");
+            for (int i = 0; i < 5; i++) {
+                writer.write("\t" + stats[i] + " ms.\t");
+            }
+        }
+
+        writer.flush();
+        writer.close();
+    }
+
+    /**
+     * Auxiliary method for writeStatistics. The returned array will contain the
+     * information of the time taken by transforming the data, writing the WTF file,
+     * reading the wtf file, inverse transforming the data, writing the bmp file and
+     * the length of the wtf file in bytes. NOTICE: This method will write files on
+     * the disk and delete them. The file names are specified as constants
+     * STATS_TEMP_WTF_FILE_NAME and STATS_TEMP_BMP_FILE_NAME. If those files exist
+     * already, they will be overwritten and deleted.
+     * 
+     * @param data The data to be transformed
+     * @param lol The level of loss
+     * @return 
+     * @throws FileNotFoundException
+     * @throws IOException 
+     */
+    
+    private static long[] convertAndAnalyze(byte[][][] data, int lol) throws FileNotFoundException, IOException {
+        long[] stats = new long[6];
+        long time = System.currentTimeMillis();
+        int originalHeight = data[0][0].length;
+
+        int[][][] transform = HaarTransform.lossyTransfrom(data, lol);
+        stats[0] = System.currentTimeMillis() - time;
+        time = System.currentTimeMillis();
+
+        File tempWtf = new File(STATS_TEMP_WTF_FILE_NAME);
+        WTFIO.writeMixedData(transform, originalHeight, lol, tempWtf);
+        stats[1] = System.currentTimeMillis() - time;
+        time = System.currentTimeMillis();
+
+        WTFIO readWtf = new WTFIO(tempWtf);
+        int[][][] readTransform = readWtf.readData();
+        stats[2] = System.currentTimeMillis() - time;
+        time = System.currentTimeMillis();
+
+        byte[][][] newData = HaarTransform.inverseLossyTransform(readTransform, readWtf.getOriginalHeight(), readWtf.getLevelOfLoss());
+        stats[3] = System.currentTimeMillis() - time;
+        time = System.currentTimeMillis();
+
+        File tempBmp = new File(STATS_TEMP_BMP_FILE_NAME);
+        BitmapIO.writeByteDataIntoBitmap(data, tempBmp);
+        stats[4] = System.currentTimeMillis() - time;
+        
+        stats[5] = tempWtf.length();
+
+        tempBmp.delete();
+        tempWtf.delete();
+
+        return stats;
     }
 }
